ea1af12d562ec52c8849054671d7ddc10853ab64
AISHA-PRO <aisha1rashed@gmail.com>
2019-12-16 03:47:29 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ export default class App extends React.Component{
     componentDidMount(){
         fetch('http://api.open-notify.org/astros.json')
         .then(response=>response.json())
-        .then(data=>{
-            console.log(data)
-        })
+        .then(data=>{ console.log(data) })
         .catch(error=>console.log(error));
     }
     render(){
         return(
             <div>
-                
+
             </div>
         );
     }
